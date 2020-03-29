18a2b8d508a2702d747be99996a79535275829af
xiatian-00 <848122841@qq.com>
2020-03-29 17:06:37 +0800

    1、添加swagger注解。 2、实现我的社团查询接口

--- club-server/src/main/java/com/graduate/club/api/UserClubApi.java
+++ club-server/src/main/java/com/graduate/club/api/UserClubApi.java
@@ -1,11 +1,33 @@
 package com.graduate.club.api;
 
+import com.graduate.club.entity.User;
+import com.graduate.club.entity.UserClub;
+import com.graduate.club.service.UserClubService;
+import com.graduate.club.vo.ResultVO;
+import com.graduate.club.vo.UserVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/review")
+@Api(value = "用户社团",tags ="UserClubApi" )
 @Slf4j
 public class UserClubApi {
+    @Autowired
+    private UserClubService userClubService;
+
+    @ApiOperation(value = "查询用户社团")
+    @RequestMapping(value = "selectMyClub",method = RequestMethod.POST)
+    public ResultVO selectMyClub(@ApiParam(name = "userId",value = "用户id",required = true) String userId ) {
+        ResultVO resultVO = userClubService.selectMyClub(userId);
+        return resultVO;
+    }
+
 }
